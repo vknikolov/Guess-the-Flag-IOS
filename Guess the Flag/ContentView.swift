128500eeceba649c924d3ae0bbf9f952d59ad2b3
Veselin <Nikolov>
2025-09-29 17:10:51 +0300

    Add custom modifier.

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct FlagImage: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .clipShape(.capsule)
+            .shadow(radius: 5)
+    }
+}
+
+extension View {
+    func imageStyle() -> some View {
+        modifier(FlagImage())
+    }
+}
+
 struct ContentView: View {
     @State private var countries = [
         "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland",
@@ -62,8 +76,7 @@ struct ContentView: View {
                             flagTapped(number)
                         } label: {
                             Image(countries[number])
-                                .clipShape(.capsule)
-                                .shadow(radius: 5)
+                                .imageStyle()
                         }
 
                     }
